052d287e8fb354d6a5377e7e626ea2725992d907
Arnab Das <das92.arnab@gmail.com>
2024-05-27 01:00:47 +0530

    feat: Added login and file-upload skeleton components and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then(x => x.LoginComponent)
+    },
+    {
+        path: 'upload',
+        loadComponent: () => import('./file-upload/file-upload.component').then(x => x.FileUploadComponent)
+    }
+];
