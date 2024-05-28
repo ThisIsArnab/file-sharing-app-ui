f652345d06a3153ae0013f31819b613802b3811a
Arnab Das <das92.arnab@gmail.com>
2024-05-28 22:01:38 +0530

    feat: added file list view to file-upload component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
     selector: 'app-file-upload',
     standalone: true,
-    imports: [MatInputModule],
+    imports: [MatInputModule, MatButtonModule, MatIconModule],
     templateUrl: './file-upload.component.html',
     styleUrl: './file-upload.component.scss'
 })
 export class FileUploadComponent {
+
+    files: Array<File> = [];
 
     dropHandler(event: DragEvent) {
         console.log('Drop Event', event);
@@ -23,12 +28,16 @@ export class FileUploadComponent {
                 // If dropped items aren't files, reject them
                 if (item.kind === "file") {
                     const file = item.getAsFile();
+                    if (file) {
+                        this.files.push(file);
+                    }
                     console.log(`… file[${i}].name = ${file?.name}`);
                 }
             });
         } else if (event.dataTransfer?.files) {
             // Use DataTransfer interface to access the file(s)
             [...event.dataTransfer.files].forEach((file, i) => {
+                this.files.push(file);
                 console.log(`file[${i}].name = ${file.name}`);
             });
         }
@@ -37,5 +46,9 @@ export class FileUploadComponent {
     dragOverHandler(event: DragEvent) {
         event.preventDefault();
         // console.log('Dragover event', event);
+    }
+
+    removeFile(file: File) {
+        
     }
 }
